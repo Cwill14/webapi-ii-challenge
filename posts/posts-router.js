06a33baa3778a4c2cae2855eabfe36bfe42b5d86
Clark Williams <theclarkwilliams@gmail.com>
2019-08-06 14:30:14 -0600

    get by id working

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -2,8 +2,6 @@ const router = require('express').Router();
 
 const Data = require('../data/db.js');
 
-router.post
-
 router.get('/', (req, res) => {
     Data.find()
         .then(data => {
@@ -29,11 +27,25 @@ router.post('/', (req, res) => {
     }
 })
 
-router.get
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    // const id = req.params.id;
 
-router.delete
+    if(id) {
+        Data.findById(id)
+            .then(post => {
+                res.status(200).json(post)
+            })
+            .catch(error => {
+                res.status(500).json({ error: "The post information could not be retrieved." })
+            })
+    } else {
+        res.status(404).json({ message: "The post with the specified ID does not exist." })
+    }
+})
+// router.delete('/:id')
 
-router.put
+// router.put('/:id')
 
 
 module.exports = router;
