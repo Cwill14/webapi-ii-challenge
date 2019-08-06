804af9f0bba56dc8d0660c3662d1f0e783afbe0c
Clark Williams <theclarkwilliams@gmail.com>
2019-08-06 14:19:49 -0600

    post to posts working

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -14,6 +14,21 @@ router.get('/', (req, res) => {
         })
 })
 
+router.post('/', (req, res) => {
+    const postBody = req.body;
+    if (postBody.title && postBody.contents) {
+        Data.insert(postBody)
+            .then(post => {
+                res.status(201).json(post)
+            })
+            .catch(error => {
+                res.status(500).json({ error: "There was an error while saving the post to the database" })
+            })
+    } else {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    }
+})
+
 router.get
 
 router.delete
